a3a8b85c2fe54570991061bf7a586b6223a52ca5
anilkusc <anilkuscu95@gmail.com>
2021-12-13 00:03:37 +0300

    feat: :sparkles: added new consensus algorithm

--- blockchain.py
+++ blockchain.py
@@ -1,8 +1,11 @@
 import hashlib
 import json
-from time import time
+import time
+from flask.json import jsonify
 import requests
 from flask.globals import request
+import random
+import socket
 
 # Blockchain class which has all attributes of the blockchain network
 class Blockchain(object):
@@ -15,20 +18,27 @@ class Blockchain(object):
         # transactions are used for sending and receiving money. Every block record transactions. 
         # For ex. if you want to check balance of your wallet , you need to query all of your questions and get the result.
         self.transactions = []
+        # it is stated for election records.
+        self.elections = []
         # Create genesis(first) block.
-        self.new_block(previous_hash=0,proof=100)
+        self.new_block(previous_hash=0)
+        # get self address for node
+        self.address = socket.gethostbyname(socket.gethostname())
+        # when elect state true all transaction are stopped and chain create new block.
+        self.elect_state = False
+        # election finishes when deadline occured
+        # it must be epoch time.
+        self.election_deadline = 0
     # this function adds new blocks to the chain
-    def new_block(self,previous_hash,proof):
+    def new_block(self,previous_hash):
         # sealing last block. After sealing new empty new block will be added to block and this block cannot be tampered.
         block = {
             # index of the block
             "index": len(self.chain) + 1,
             # creating time as epoch time.
-            "timestamp": time() ,
+            "timestamp": time.time() ,
             # all transactions of the last block.
             "transactions": self.transactions,
-            # proof of the block
-            "proof": proof,
             # hash of the previous block. If you change anything on any block all hashes will be change. So it is the proof of consistency
             "previous_hash": previous_hash
         }
@@ -58,19 +68,6 @@ class Blockchain(object):
     def hash(self,block):
         block_string = json.dumps(block,sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
-    # proof of work mining
-    def proof_of_work(self,last_proof):
-        # try all possibilites and brute force to proof
-        proof = 0
-        while self.valid_proof(last_proof,proof) is False:
-            proof = proof + 1
-        # when you find the proof return it
-        return proof
-    # validation of the proof
-    def valid_proof(self,last_proof,proof):
-        guess = f'{last_proof}{proof}'.encode()
-        guess_hash = hashlib.sha256(guess).hexdigest()
-        return guess_hash[:4] == "0000"
     # just add node to nodes array
     def register_node(self, address):
         self.nodes.append(address)
@@ -80,8 +77,6 @@ class Blockchain(object):
         control_block = chain[0]
         for block in chain:
             if block["previous_hash"] != self.hash(control_block):
-                return False
-            if not self.valid_proof(control_block['proof'], block['proof']):
                 return False
         return True
     # Consensus Algorithm. For now it longest one would be winner and the longest one would be authorative
@@ -99,7 +94,7 @@ class Blockchain(object):
                 # get neighbour's chain length and chains.
                 length = response.json()['length']
                 chain = response.json()['chain']
-            # If nighbours node is longer than ours we consider it authoritive node and replicate information from the neighbour.
+            # If neighbours node is longer than ours we consider it authoritive node and replicate information from the neighbour.
             if self.valid_node(chain) and length > max_length:
                 new_chain = chain
                 max_length = len(new_chain)
@@ -107,3 +102,45 @@ class Blockchain(object):
             self.chain = new_chain
             return True
         return False
+    # when transactions has been done , blockchain's state set to election state.
+    # Every node elect a node randomly and stream it to the blockchain network.
+    def elect_state_sender(self):
+        vote = random.randint(0,len(self.nodes)-1)
+        election_record = {"vote":vote,"node":self.address}
+        # send every node to my vote
+        for node in self.nodes:
+            r = requests.post("http://"+node+":5000/election/vote", data = election_record)
+            if r.status_code == 200:
+                print("Stream has been sent to node:"+node)
+            else:
+                print("Stream could not sent to node:"+node)
+    def leader_control(self):
+        max = 0
+        leader = self.nodes[0]
+        for node in self.nodes:
+            if self.nodes.count(node) > max:
+                max = self.nodes.count(node)
+                leader = node
+            elif self.nodes.count(node) == max:
+                # if vote counts are equal , select min of two nodes.
+                leader = min(leader,node)
+        return leader
+    # this functions control if this node is leader or not.
+    # If leader is ownself , send stream to network
+    def declare_leader_control(self):
+        while 1:
+            if round(time.time()) < self.election_deadline :
+                print("Chain Network is in elect state. Waiting for leader control.")
+                time.sleep(5)
+            else:
+                self.elect_state = False
+                if self.address == self.leader_control():
+                    for node in self.nodes:
+                        mine_data = {"leader":self.address,"receiver":self.address}
+                        r = requests.post("http://"+node+":5000/mine", json = mine_data)
+                        if r.status_code == 200:
+                            print("Stream has been sent to node:"+node)
+                        else:
+                            print("Stream could not sent to node:"+node)
+                print("The node is no more in elect state.")    
+                break
--- main.py
+++ main.py
@@ -2,45 +2,65 @@ import json
 from flask import Flask, jsonify, request
 from uuid import uuid4
 from blockchain import Blockchain
-
+import time
+import _thread
 # Generate a globally unique address for this node
 node_identifier = str(uuid4()).replace('-', '')
 
 app = Flask(__name__)
 # create my chain
 myblockchain = Blockchain()
-#mining endpoint
-@app.route('/mine', methods=['GET'])
+# adding self address to nodes
+myblockchain.nodes.append(myblockchain.address)
+#mining endpoint.Selected node will be mining.
+@app.route('/mine', methods=['POST'])
 def mine():
-    # proof of working for mining
-    last_block = myblockchain.last_block()
-    last_proof = last_block["proof"]
-    proof = myblockchain.proof_of_work(last_proof)
-    # because we adding new block, sender is 0.
-    myblockchain.new_transaction(sender=0,receiver=node_identifier,amount=1)
-    # hash the last block for consistency
-    previous_hash = myblockchain.hash(last_block)
-    # new block will be created and it will return the information of the new block.
-    block = myblockchain.new_block(proof,previous_hash)
-    response = {
-        "message": "New Block Forged",
-        "index": block["index"],
-        "transactions": block["transactions"],
-        "proof": block["proof"],
-        "previous_hash" : block["previous_hash"]
-    }
-
-    return jsonify(response), 200
+    values = request.get_json()
+    code = 200
+    if values["leader"] == myblockchain.leader_control():
+        # proof of working for mining
+        last_block = myblockchain.last_block()
+        # hash the last block for consistency
+        previous_hash = myblockchain.hash(last_block)
+        # new block will be created and it will return the information of the new block.
+        block = myblockchain.new_block(previous_hash)
+        # because we adding new block, sender is 0.
+        myblockchain.new_transaction(sender=0,receiver=values["receiver"],amount=1)
+        response = {
+            "message": "New Block Forged",
+            "index": block["index"],
+            "transactions": block["transactions"],
+            "previous_hash" : block["previous_hash"]
+        }
+    else:
+        response = {'message': f'Leader is not legit.'}
+        code = 400
+    return jsonify(response), code
 # create new transaction
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
+    if myblockchain.elect_state == True:
+        response = {'message': f'Chain is currently in elect state.Please wait...'}
+        return jsonify(response), 301
     values = request.get_json()
     # control if sender,reciver,amount values are specified , otherwise return error
     if values["sender"] == None or values["receiver"] == None or values["amount"] == None:
         return 'At least one of these variables are missing: sender,receiver,amount', 400
     # create new transaction on the block
     index = myblockchain.new_transaction(values["sender"],values["receiver"],values["amount"])
-    response = {'message': f'Transaction will be added to Block {index}'}
+    # If record count is reach to 10 , all chain is stop transactions immediately,
+    # they have synchronize all transactions and starte elections state.
+    # After election on of the node would be elect for creating new block and transactions are continue.
+    if len(myblockchain.transactions) > 3:
+        response = {'message': f'Transaction will be added to Block {index}. The transaction limit has been reached for this block. New Block will be created in short time.'}
+        myblockchain.elect_state = True
+        myblockchain.election_deadline = round(time.time()) + 3
+        try:
+            _thread.start_new_thread( myblockchain.declare_leader_control )
+        except:
+            print ("Error: unable to start thread: declare_leader_control")
+    else:
+        response = {'message': f'Transaction will be added to Block {index}.'}
     return jsonify(response), 201
 # get the all information of chains
 @app.route('/chain', methods=['GET'])
@@ -63,7 +83,7 @@ def register_nodes():
     response = {
         'message': 'New nodes have been added',
         'total_nodes': list(myblockchain.nodes),
-    }        
+    }
     return jsonify(response), 200
 # control if there is any conflict between our blockchain node and other nodes.
 @app.route('/nodes/resolve', methods=['GET'])
@@ -79,6 +99,24 @@ def resolve_conflicts():
             'chain': myblockchain.chain
         }
     return jsonify(response), 200
+
+@app.route('/election/vote', methods=['POST'])
+def election():
+    values = request.get_json()
+    code = 400
+    if round(time.time()) > myblockchain.election_deadline or myblockchain.elect_state == False:
+        response = {'message': 'Election time has been finished:'}
+    elif values["node"] == None or values["vote"] == None:
+        response = {'message': 'Vote is invalid'}
+    elif values["node"] not in myblockchain.nodes:
+        response = {'message': 'Node is not registered'}
+    elif values["vote"] < 0 or values["vote"] > (len(myblockchain.nodes)-1):
+        response = {'message': 'Node is not registered'}
+    else:
+        myblockchain.elections.append(values)
+        response = {'message': 'Vote is received.{values}'}
+        code = 200
+    return jsonify(response), code
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
